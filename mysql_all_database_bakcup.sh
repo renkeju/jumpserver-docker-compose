518aa64499068030c5d02fdcbc97fa258f2ef818
windtook <renkeju@gmail.com>
2019-05-10 14:04:48 +0800

    modify mysql database backup bash script

--- mysql_all_database_bakcup.sh
+++ mysql_all_database_bakcup.sh
@@ -5,4 +5,4 @@ BACKUP_DIRECTORY="./db_backup"
 [ -d ${BACKUP_DIRECTORY} ] || mkdir -p ${BACKUP_DIRECTORY}
 [ -f ${BACKUP_DIRECTORY}//all-database_${TODAY_DATE}.sql ] && rm -vrf ${BACKUP_DIRECTORY}/all-database_${TODAY_DATE}.sql
 
-docker exec jumpserver_database_1 sh -c "exec mysqldump --all-databases -uroot -p${MYSQL_ROOT_PASSWORD}" > ${BACKUP_DIRECTORY}/all-database_${TODAY_DATE}.sql
+docker exec jumpserver-docker-compose_mysql_1 sh -c "exec mysqldump --all-databases -uroot -p${MYSQL_ROOT_PASSWORD}" > ${BACKUP_DIRECTORY}/all-database_${TODAY_DATE}.sql
